d56088e7e4de458880587ada8e0f56dd493cad04
Florent <>
2018-05-19 12:26:22 +0200

    fix

--- app/Http/Controllers/API/ClientController.php
+++ app/Http/Controllers/API/ClientController.php
@@ -2,159 +2,161 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Http\Controllers\Controller;
+use App\Model\Client;
+use App\Model\ClientProfil;
+use App\Model\Wine;
+use App\Model\WineDrink;
+use App\Model\WineFav;
 use DB;
+use Illuminate\Http\Request;
 use JWTAuth;
 use \Validator;
-use App\Model\Client;
-use App\Model\Wine;
-use App\Model\WineFav;
-use App\Model\WineDrink;
-use App\Model\ClientProfil;
-use App\Http\APIResponse;
-use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
-use Tymon\JWTAuth\Exceptions\JWTException;
 
 class ClientController extends Controller
 {
-	public function signup(Request $request){
-    $data = [];
+    public function signup(Request $request)
+    {
+        $data = [];
 
-		if($request->isMethod("POST")){
-			$valid = Validator($request->all(),
-				[
-					'email' => 'required|email|max:75',
-					'password' =>'required|max:100'
-				]);
+        if ($request->isMethod("POST")) {
+            $valid = Validator($request->all(),
+                [
+                    'email' => 'required|email|max:75',
+                    'password' => 'required|max:100',
+                ]);
 
-			if($valid->fails()){
-				$data["alert"] = [
-					"type" => "fail",
-					"message" => "Merci de compléter tous les champs."
-				];
-				return $data;
-      }
+            if ($valid->fails()) {
+                $data["alert"] = [
+                    "type" => "fail",
+                    "message" => "Merci de compléter tous les champs.",
+                ];
+                return $data;
+            }
 
-      $clientExist = Client::where("email", "=", $request->input('email'))->where("password", "=", md5($request->input('password')))->first();
-      if($clientExist) {
-        $data["alert"] = [
-					"type" => "fail",
-					"message" => "Client déjà existant !"
-				];
-				return $data;
-      }
-			DB::beginTransaction();
-			$error = 0;
+            $clientExist = Client::where("email", "=", $request->input('email'))->where("password", "=", md5($request->input('password')))->first();
+            if ($clientExist) {
+                $data["alert"] = [
+                    "type" => "fail",
+                    "message" => "Client déjà existant !",
+                ];
+                return $data;
+            }
+            DB::beginTransaction();
+            $error = 0;
 
-			$client = new Client();
-			$client->email = $request->input("email");
-			$client->password = md5($request->input("password"));
-			$client->date_creation = date("Y-m-d H:i:s");
+            $client = new Client();
+            $client->email = $request->input("email");
+            $client->password = md5($request->input("password"));
+            $client->date_creation = date("Y-m-d H:i:s");
 
-			if(!$client->save())
-				$error++;
+            if (!$client->save()) {
+                $error++;
+            }
 
-			if($error>=1){
-				DB::rollback();
-				$data["alert"] = [
-					"type" => "fail",
-					"message" => "Une erreur est survenue durant l'ajout de l'utilisateur. Merci de réessayer ultérieurement."
-				];
-				return $data;
-			}
+            if ($error >= 1) {
+                DB::rollback();
+                $data["alert"] = [
+                    "type" => "fail",
+                    "message" => "Une erreur est survenue durant l'ajout de l'utilisateur. Merci de réessayer ultérieurement.",
+                ];
+                return $data;
+            }
 
-			DB::commit();
-			$data["alert"] = [
-				"type" => "success",
-			];
-			return $data;
-		}
+            DB::commit();
+            $data["alert"] = [
+                "type" => "success",
+            ];
+            return $data;
+        }
 
-		return $data;
-  }
-  
-  public function login(Request $request){
-      $data = [];
-      if($request->isMethod('POST')){
-          // FORM VALIDATION
-          $validator = Validator::make($request->all(), [
-              'email' => 'required|email',
-              'password' => 'required',
-          ]);
-          if ($validator->fails()){
-              $data["alert"] = [
-                  "type" => "fail",
-                  "message" => $validator->errors()->first()
-              ];
-              return $data;
-          }
+        return $data;
+    }
 
-          // user verification
-		  $client = Client::where("email", "=", $request->input('email'))
-							->where("password", "=", md5($request->input('password')))
-							->first();
+    public function login(Request $request)
+    {
+        $data = [];
+        if ($request->isMethod('POST')) {
+            // FORM VALIDATION
+            $validator = Validator::make($request->all(), [
+                'email' => 'required|email',
+                'password' => 'required',
+            ]);
+            if ($validator->fails()) {
+                $data["alert"] = [
+                    "type" => "fail",
+                    "message" => $validator->errors()->first(),
+                ];
+                return $data;
+            }
 
-		  $client_profil = ClientProfil::where("client_id", "=", $client->id)->first();
+            // user verification
+            $client = Client::where("email", "=", $request->input('email'))
+                ->where("password", "=", md5($request->input('password')))
+                ->first();
 
-          if(is_null($client)){
-              $data["alert"] = [
-                  "type" => "fail",
-                  "message" => "Votre adresse email ou mot de passe est invalide."
-              ];
-              return $data;
-		  }
+            $client_profil = ClientProfil::where("client_id", "=", $client->id)->first();
 
-		  $token = JWTAuth::fromUser($client);
-		  $client->password = "";
+            if (is_null($client)) {
+                $data["alert"] = [
+                    "type" => "fail",
+                    "message" => "Votre adresse email ou mot de passe est invalide.",
+                ];
+                return $data;
+            }
 
-		  if(!is_null($client_profil)){
-			$winesProfil = [];
-			$winesDrink = [];
-			$winesFav = [];
-			$drinkAll = WineDrink::get()->all();
-			$favAll = WineFav::get()->all();
-			$winesAll = Wine::get()->all();
+            $token = JWTAuth::fromUser($client);
+            $client->password = "";
 
-			foreach($winesAll as $wine) {
-				if($wine->rProfil()->first()->profil_id == $client_profil->profil_id) {
-					array_push($winesProfil, $wine);
-				}
-			}
-			foreach($drinkAll as $drink) {
-				
-				if($drink->user_id == $client->id) {
-					array_push($winesDrink, $drink->rWines()->first());
-				}
-			}
-			foreach($favAll as $fav) {
-				
-				if($fav->user_id == $client->id) {
-					array_push($winesFav, $fav->rWines()->first());
-				}
-			}
+            if (!is_null($client_profil)) {
+                $winesProfil = [];
+                $winesDrink = [];
+                $winesFav = [];
+                $drinkAll = WineDrink::get()->all();
+                $favAll = WineFav::get()->all();
+                $winesAll = Wine::get()->all();
 
-			$data["alert"] = [
-				"type" => "success",
-				"profil" => true,
-				"user" => $client,
-				"winesProfil" => $winesProfil,
-				"winesFav" => $winesFav,
-				"winesDrink" => $winesDrink,
-				"winesAll" => $winesAll,
-				"token" => $token
-			];
-			return $data;
-		}
-		  
+                foreach ($winesAll as $wine) {
+                    if ($wine->rProfil()->first()->profil_id == $client_profil->profil_id) {
+                        array_push($winesProfil, $wine);
+                    }
+                }
+                foreach ($drinkAll as $drink) {
 
-          $data["alert"] = [
-						"type" => "success",
-						"profil" => false,
-						"user" => $client,
-						"token" => $token
-          ];
-      }
+                    if ($drink->user_id == $client->id) {
+                        array_push($winesDrink, $drink->rWines()->first());
+                    }
+                }
+                foreach ($favAll as $fav) {
 
-      return $data;
-  }
+                    if ($fav->user_id == $client->id) {
+                        array_push($winesFav, $fav->rWines()->first());
+                    }
+                }
+
+                $data["alert"] = [
+                    "type" => "success",
+                    "profil" => true,
+                    "user" => $client,
+                    "winesProfil" => $winesProfil,
+                    "winesFav" => $winesFav,
+                    "winesDrink" => $winesDrink,
+                    "winesAll" => $winesAll,
+                    "token" => $token,
+                ];
+                // return $data;
+                return response()->json(utf8_encode(json_encode($data)));
+            }
+
+            $data["alert"] = [
+                "type" => "success",
+                "profil" => false,
+                "user" => $client,
+                "token" => $token,
+            ];
+        }
+
+        // return $data;
+        return response()->json(utf8_encode(json_encode($data)));
+    }
 }
